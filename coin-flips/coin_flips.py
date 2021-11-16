ba50b2161f412ef7027d8f68f7ad8bc806c046e6
Michael Tan <thr205@hotmail.com>
2021-11-16 20:48:03 +0800

    finish hw 6

--- coin-flips/coin_flips.py
+++ coin-flips/coin_flips.py
@@ -22,11 +22,13 @@ class coin:
     def __str__(self) -> str:
         print("The coin is filped with {} times.\nHead: {}, Tail: {}.\n".format(self.head + self.tail, self.head, self.tail))
 
-counts = 20
+flip_counts = 20
+coin_counts = 2
+threshold = 3.84
 
-coins = [coin() for _ in range(2)]
+coins = [coin() for _ in range(coin_counts)]
 for piece in coins:
-    piece.flip(counts)
+    piece.flip(flip_counts)
 
 for piece in coins:
     piece.__str__()
@@ -38,8 +40,24 @@ for piece in coins:
     results.append([piece.head, piece.tail])
 
 results = pd.DataFrame(results, index=['Coin 1', 'Coin 2'], columns=['Head', 'Tail'])
-joint_probability = results / 40
+joint_probability = results / (flip_counts * coin_counts)
 print(joint_probability)
 
+print('\nThe marginal probability: ')
 print('P(Head)={:.3f}'.format(joint_probability['Head'].sum()))
 print('P(Tail)={:.3f}'.format(joint_probability['Tail'].sum()))
+print('P(Coin 1)={:.3f}'.format(joint_probability.iloc[0, :].sum()))
+print('P(Coin 2)={:.3f}'.format(joint_probability.iloc[1, :].sum()))
+
+chi_square_freedom = 0
+for i in range(coin_counts):
+    expectation_head = (flip_counts * coin_counts) * joint_probability['Head'].sum() * joint_probability.iloc[i, :].sum()
+    expectation_tail = (flip_counts * coin_counts) * joint_probability['Head'].sum() * joint_probability.iloc[i, :].sum()
+    chi_square_freedom += ((results.iloc[i, 0] - expectation_head) ** 2 / expectation_head + (results.iloc[i, 1] - expectation_tail) ** 2 / expectation_tail)
+
+if chi_square_freedom < threshold:
+    print('The chi-square freedom is: {:.3f}, which is smaller than the threshold {}.'.format(chi_square_freedom, threshold))
+    print('Independent')
+else:
+    print('The chi-square freedom is: {:.3f}, which is bigger than the threshold {}.'.format(chi_square_freedom, threshold))
+    print('Not independent')
